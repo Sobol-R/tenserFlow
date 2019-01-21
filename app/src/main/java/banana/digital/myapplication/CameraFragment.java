b88372adaafb09b169878b6c50e1748e421eedd1
Banandroid Z <ndrbnnz@gmail.com>
2019-01-21 21:23:00 +0300

    done.1

--- app/src/main/java/banana/digital/myapplication/CameraFragment.java
+++ app/src/main/java/banana/digital/myapplication/CameraFragment.java
@@ -4,30 +4,48 @@ package banana.digital.myapplication;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.PermissionRequest;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Detector;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class CameraFragment extends Fragment {
@@ -42,6 +60,7 @@ public class CameraFragment extends Fragment {
     private HandlerThread backgroundThread;
     private Handler backgroundHandler;
 
+    private FirebaseVisionLabelDetector firebaseVisionLabelDetector;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,6 +79,7 @@ public class CameraFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        initializeDetector();
         if (textureView.isAvailable()) checkPermission();
         else {
             textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
@@ -84,6 +104,14 @@ public class CameraFragment extends Fragment {
                 }
             });
         }
+    }
+
+    private void initializeDetector() {
+        FirebaseVisionLabelDetectorOptions options =
+                new FirebaseVisionLabelDetectorOptions.Builder()
+                        .setConfidenceThreshold(0.8f)
+                        .build();
+        firebaseVisionLabelDetector = FirebaseVision.getInstance().getVisionLabelDetector(options);
     }
 
     private void checkPermission() {
@@ -148,8 +176,67 @@ public class CameraFragment extends Fragment {
         }
     }
 
-    private void captureCamera(@NonNull CameraDevice cameraDevice) {
+    private void captureCamera(@NonNull final CameraDevice cameraDevice) {
+        final Surface surface = new Surface(textureView.getSurfaceTexture());
+        final List<Surface> surfaces = Arrays.asList(surface);
+        try {
+            cameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    startRecord(cameraDevice, cameraCaptureSession, surface);
+                }
 
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+
+                }
+            }, backgroundHandler);
+        } catch (CameraAccessException e) {
+
+        }
+    }
+
+    private void startRecord(final CameraDevice cameraDevice, CameraCaptureSession cameraCaptureSession, Surface surface) {
+        try {
+            CaptureRequest.Builder requestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            requestBuilder.addTarget(surface);
+            requestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
+            cameraCaptureSession.setRepeatingRequest(requestBuilder.build(), new CameraCaptureSession.CaptureCallback(){
+                @Override
+                public void onCaptureStarted(@androidx.annotation.NonNull @NonNull CameraCaptureSession session, @androidx.annotation.NonNull @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+                    firebaseStartLabelDetecting();
+                }
+            }, backgroundHandler);
+        } catch (CameraAccessException e) {
+
+        }
+    }
+
+    private boolean firebaseLabelDetectingStarted = false;
+
+    private void firebaseStartLabelDetecting() {
+        if (!firebaseLabelDetectingStarted) {
+            firebaseLabelDetectingStarted = true;
+            processFirebaseLabelDetecting();
+        }
+    }
+
+    private void processFirebaseLabelDetecting() {
+        Bitmap bitmap = textureView.getBitmap();
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        firebaseVisionLabelDetector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionLabel> firebaseVisionLabels) {
+                        final List<String> labels = new ArrayList<>();
+                        for (FirebaseVisionLabel visionLabel : firebaseVisionLabels) {
+                            labels.add(visionLabel.getLabel());
+                        }
+                        textView.setText(TextUtils.join(",", labels));
+                        processFirebaseLabelDetecting();
+                    }
+                });
     }
 
 }
